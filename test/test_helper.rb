cc19e8781a38d51dd9bb612788176270a76146f3
trantuan <chenjun6403@163.com>
2021-08-19 13:00:06 +0800

    添加 Guard 自动测试

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+
+# Conflict with Jetbrains Test Module
+# Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
